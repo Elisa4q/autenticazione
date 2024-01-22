3e8019eab3713951ca764cb3b0d2f04c5e9df2aa
Elisa4q <elisa.santomauro@iismajoranamoncalieri.edu.it>
2024-01-22 10:57:04 +0100

    modifiche

---
+++ inserisci_login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang = "it">
+    <head></head>
+    <body>
+     
+<?php
+    print"<form method = 'post' action ='login.php'>";
+    print"Username: <input type = 'text' name = 'user' sixe = '10'><br><br>";
+    print"Password: <input type = 'text' name = 'psw' sixe = '10'><br><br>";
+    print"<input type = 'submit' value = 'login'> &nbsp; &nbsp; &nbsp;";
+    print"<input type = 'reset' value = 'Reset'>";
+    print"</form>";
+?>
+</body>
+</hmtl>
+ 
